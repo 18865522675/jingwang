3dba4f9807e482837c67bab6e2de7d472a88ca27
cmy <1259108412@qq.com>
2019-01-30 16:44:09 +0800

    zbj

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -308,7 +308,6 @@ export default {
             .craftContent-imgSize {
               width: 100%;
               height: 247px;
-              background: red;
               img {
                 width: 100%;
                 height: 100%;
