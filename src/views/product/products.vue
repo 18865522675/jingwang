72ef1e7724cccd39ce7ba456dbd97770e5e9baf8
cmy <1259108412@qq.com>
2019-01-30 11:41:36 +0800

    zbjsz

--- src/views/product/products.vue
+++ src/views/product/products.vue
@@ -30,7 +30,7 @@
                 <div class="left-contentDetails-img">
                   <img src="../../assets/切图/-s-JWN-A.png" alt="">
                 </div>
-                <h3>制氮JWN标准型</h3>
+                <h3>制氮JWN-A型</h3>
                 <h5>应用领域: <span style="font-size:14px;font-weight: normal;">适用于石油、化工、SMT、食品加工、医药及器械、粉末冶金、铜加工、天然气、金属热处理等等</span></h5>
                 <div style="text-align: left; margin-top: 10px;">
                   <!-- <router-link to="/productDetails"> -->
@@ -42,7 +42,7 @@
                 <div class="left-contentDetails-img">
                   <img src="../../assets/切图/-s-JWN-B.png" alt="">
                 </div>
-                <h3>制氮JWN标准型</h3>
+                <h3>制氮JWN-B型</h3>
                 <h5>应用领域: <span style="font-size:14px;font-weight: normal;">适用于石油、化工、SMT、食品加工、医药及器械、粉末冶金、铜加工、天然气、金属热处理等等</span></h5>
                 <div style="text-align: left; margin-top: 10px;">
                   <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看详情</el-button>
@@ -52,7 +52,7 @@
                 <div class="left-contentDetails-img">
                   <img src="../../assets/切图/-s-JWN-C.png" alt="">
                 </div>
-                <h3>制氮JWN标准型</h3>
+                <h3>制氮JWN-C型</h3>
                 <h5>应用领域: <span style="font-size:14px;font-weight: normal;">适用于石油、化工、SMT、食品加工、医药及器械、粉末冶金、铜加工、天然气、金属热处理等等</span></h5>
                 <div style="text-align: left; margin-top: 10px;">
                   <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看详情</el-button>
@@ -65,7 +65,7 @@
           <div class="left-content">
             <h3>制氮</h3>
             <article>
-              <div class="left-contentDetails" style="margin-right: 126px;">
+              <div class="left-contentDetails" style="margin-right: 110px;">
                 <div class="left-contentDetails-img">
                   <img src="" alt="">
                 </div>
@@ -85,7 +85,7 @@
                   <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看详情</el-button>
                 </div>
               </div>
-              <div class="left-contentDetails" style="margin-right: 126px;">
+              <div class="left-contentDetails" style="margin-right: 110px;">
                 <div class="left-contentDetails-img">
                   <img src="" alt="">
                 </div>
@@ -304,11 +304,11 @@
                 <div>
                   <img src="../../assets/切图/xifutichu.png" alt="">
                 </div>
-                <div class="technology-details-intro" style="">
+                <div class="technology-details-intro">
                   <h3 style="">变压吸附提纯氢气技术</h3>
                   <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button @click="$router.push('/techDetails')" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 1}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
@@ -317,10 +317,10 @@
                   <img src="../../assets/切图/-s-jinghuanongsuo.png" alt="">
                 </div>
                 <div class="technology-details-intro" style="">
-                  <h3 style="">变压吸附提纯氢气技术</h3>
-                  <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
+                  <h3 style="">变压吸附净化浓缩甲烷技术</h3>
+                  <div style="margin-top: 10px; font-size:18px; text-align: left;">富含甲烷的混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 2}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
@@ -329,53 +329,56 @@
                   <img src="../../assets/切图/-s-xifutichun.png" alt="">
                 </div>
                 <div class="technology-details-intro" style="">
-                  <h3 style="">变压吸附提纯氢气技术</h3>
-                  <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
+                  <h3 style="">变压吸附提纯一氧化碳技术</h3>
+                  <div style="margin-top: 10px; font-size:18px; text-align: left;">采用变压吸附工艺从含一氧化碳、氢气、氮气、甲烷、二氧化碳等组分的混合气中提纯一氧化碳。原料气进入一段PSA装置，吸附除去二氧化碳、水分及微量...</div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 3}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
             </article>
           </div>
         </el-tab-pane>
-        <el-tab-pane label="能源煤化工技术">
+        <el-tab-pane label="能源煤化工技术" style="overflow: hidden">
           <div class="left-content">
-            <h3>环保变压吸附技术</h3>
+            <h3>天然气制氢技术</h3>
             <article>
               <div class="technology-details">
                 <div>
-                  <img src="../../assets/切图/xifutichu.png" alt="">
+                  <img src="../../assets/切图/trq.jpg" style="width:307px;">
                 </div>
                 <div class="technology-details-intro" style="">
                   <h3 style="">变压吸附提纯氢气技术</h3>
-                  <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
+                  <div style="margin-top: 10px; font-size:18px; text-align: left;">以天然气为原料，经压缩、脱硫后与水蒸气进行转化、变换反应，制得氢含量大于70%（v/v%）以上的富氢转化气。富氢转化气经变压吸附(PSA)单元分离提纯即得到纯度99.9%（v/v%）以上的产品氢气。</div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 4}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
               <div class="technology-details">
                 <div>
-                  <img src="../../assets/切图/-s-jinghuanongsuo.png" alt="">
+                  <img src="../../assets/切图/jlqz.jpg" style="width:307px;">
                 </div>
                 <div class="technology-details-intro" style="">
-                  <h3 style="">变压吸附提纯氢气技术</h3>
-                  <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
+                  <h3 style="">焦炉气制CNG/LNG技术</h3>
+                  <div style="margin-top: 10px; font-size:18px; text-align: left;">以焦炉气为原料制取天然气，为得到合格的产品（SNG/CNG/LNG）,在生产过程中，CO2的含量必须降低到规定值以下，
+                    以保证产品质量或装置稳定运行的要求，可以通过甲烷化反应、气体分离技术（如PSA）或二者的组合来实现脱除CO2同时浓缩CH4的目的。采用甲烷化的方法，不但可以降低气体中的碳氧化物，
+                    还增加了CH4含量，获得更多需要的产品。
+                  </div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 5}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
               <div class="technology-details">
                 <div>
-                  <img src="../../assets/切图/-s-xifutichun.png" alt="">
+                  <img src="../../assets/切图/jlqz.jpg" style="width:307px;">
                 </div>
                 <div class="technology-details-intro" style="">
-                  <h3 style="">变压吸附提纯氢气技术</h3>
-                  <div style="margin-top: 10px; font-size:18px; text-align: left;">含氢(H2)混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质...</div>
+                  <h3 style="">甲醇裂解制氢技术</h3>
+                  <div style="margin-top: 10px; font-size:18px; text-align: left;">甲醇、脱盐水混合后经加热汽化、过热后进入转化反应器，甲醇、水蒸汽在催化剂的作用下，在转化反应器中完成甲醇裂解、一氧化碳变换成二氧化碳等两步反应</div>
                   <div style="text-align: left; margin-top: 17px;">
-                    <el-button style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
+                    <el-button @click="$router.push({path: '/techDetails', query: {tech: 6}})" style="width:120px; height:40px; background:rgba(255,108,0,1);color: #fff">查看更多</el-button>
                   </div>
                 </div>
               </div>
@@ -473,7 +476,7 @@ export default {
     }
   }
   .content {
-    height: 1850px;
+    // height: 1850px;
     .el-tabs {
       margin-top: 22px;
       .el-tabs__header {
@@ -493,12 +496,14 @@ export default {
         .el-tab-pane {
           width: 100%;
           .left-content {
+            overflow: hidden;
             h3 {
               text-align: left;
             }
             article {
               width: 100%;
               margin-top: 5px;
+              overflow: hidden;
               border-top: 1px solid rgba(51,51,51,1);
               .left-contentDetails {
                 width: 310px;
@@ -506,7 +511,7 @@ export default {
                 float: left;
                 // border: 1px solid rgba(51,51,51,1);
                 .left-contentDetails-img {
-                  width: 240px;
+                  width: 275px;
                   height: 260px;
                   // margin: 5px auto 0;
                   img {
@@ -515,14 +520,15 @@ export default {
                   }
                 }
                 h5 {
-                  width: 240px;
+                  width: 275px;
                   margin-top: 10px;
                   text-align: left;
                 }
               }
               .technology-details {
                 width: 99%;
-                height: 270px;
+                overflow: hidden;
+                margin-bottom: 30px;
                 padding-top: 20px;
                 div {
                   width: 365px;
@@ -550,7 +556,7 @@ export default {
   .contactUs {
       width: 350px;
       position: relative;
-      top: -470px;
+      top: -380px;
     }
 }
 
--- src/views/product/techDetails.vue
+++ src/views/product/techDetails.vue
@@ -15,12 +15,15 @@
         <el-menu class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
           <el-submenu index="1">
             <template slot="title">环保变压吸附技术</template>
-            <el-menu-item index="1-1">变压吸附提纯氢气技术</el-menu-item>
-            <el-menu-item index="1-1">变压吸附净化浓缩甲烷技术</el-menu-item>
-            <el-menu-item index="1-2">变压吸附提纯一氧化碳技术</el-menu-item>
+            <el-menu-item @click="techDetail(1)" index="1-1">变压吸附提纯氢气技术</el-menu-item>
+            <el-menu-item @click="techDetail(2)" index="1-1">变压吸附净化浓缩甲烷技术</el-menu-item>
+            <el-menu-item @click="techDetail(3)" index="1-2">变压吸附提纯一氧化碳技术</el-menu-item>
           </el-submenu>
           <el-submenu index="2">
             <template slot="title">能源煤化工技术</template>
+            <el-menu-item @click="techDetail(4)" index="2-1">变压吸附提纯氢气技术</el-menu-item>
+            <!-- <el-menu-item @click="techDetail(1)" index="2-1">变压吸附提纯氢气技术</el-menu-item>
+            <el-menu-item @click="techDetail(1)" index="2-1">变压吸附提纯氢气技术</el-menu-item> -->
           </el-submenu>
         </el-menu>
         <div class="paperBottom-bottom">
@@ -58,7 +61,7 @@
 					</div>
 				</div>
       </div>
-      <div class="productDetails-contentRight">
+      <div class="productDetails-contentRight" v-if="techDetail1">
         <div class="contentRightContent clear">
           <h3>变压吸附提纯氢气技术</h3>
           <article class="contentRightWrap">
@@ -72,13 +75,13 @@
             <div class="theroyExplain" style="margin-top: 20px;"> 1. 操作灵活，技术成熟，产品质量可靠，用户信用度高。</div>
             <div class="theroyExplain top"> 2. PSA制氢业绩多，工程经验丰富。</div>
             <div class="theroyExplain top"> 3. 专利技术广泛使用，竞争力强。</div>
-            <h2>工艺原理</h2>
+            <h2>原料气</h2>
             <div class="theroyExplain top" style="margin-top: 20px;">1. 甲醇裂解气、氨裂解气、甲醇尾气、甲醛尾气</div>
             <div class="theroyExplain top">2. 变换气、精炼气、烃类蒸汽转化气，发酵气、多晶硅尾气</div>
             <div class="theroyExplain top">3. 半水煤气、城市煤气、焦炉气</div>
             <div class="theroyExplain top">4. 炼油厂催化裂化干气、炼油厂重整尾气</div>
             <div class="theroyExplain top">5. 其他含H2气源</div>
-            <h2>工艺原理</h2>
+            <h2>技术指标</h2>
             <div class="theroyExplain" style="margin-top: 20px;"> 1. 原料气： </div>
             <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用压力：0.4~4.0MPa（表压） </div>
             <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用温度：常温</div>
@@ -88,10 +91,111 @@
             <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;系统阻力降△P：0.05~0.1MPa</div>
             <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出温度：常温</div>
             <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;注： H2收率与原料组成、压力及产品以及产品H2纯度有关。</div>
-            <h2 >制氮装置部份业绩表</h2>
-            <div>
-              <img class="performanceChart" src="../../assets/img/gg.png" alt="">
+          </article>
+        </div>
+      </div>
+      <div class="productDetails-contentRight" v-if="techDetail2">
+        <div class="contentRightContent clear">
+          <h3>变压吸附净化浓缩甲烷技术</h3>
+          <article class="contentRightWrap">
+            <h2>工艺原理</h2>
+            <div class="theroyExplain" style="margin-top: 20px;">
+               富含甲烷的混合气进入变压吸附装置吸附塔后，由于吸附床内吸附剂对气体混合物中各组分的吸附能力随着压力变化而呈现差异，加压时吸附原料气中的杂质，
+               不易吸附的组分——CH4从吸附床出口端作为产品输出，得到高浓度CH4；减压时，吸附的CO2等杂质组分脱附解吸，同时吸附剂获得再生。采用至少四个吸附床循环操作，以实现产品CH4连续输出。
             </div>
+            <h2>特点及优势</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 实现了多塔任意组合和任意切换的专家控制及自适应优化控制，既提高了装置操作灵活性，又确保了PSA装置的长期、稳定、安全运行。</div>
+            <div class="theroyExplain top"> 2. 多项专利技术，竞争力强。</div>
+            <div class="theroyExplain top"> 3. PSA工程化业绩多，工程经验丰富。</div>
+            <h2>原料气</h2>
+            <div class="theroyExplain top" style="margin-top: 20px;">1. 垃圾填埋气、垃圾消化气、沼气</div>
+            <div class="theroyExplain top">2. 焦炉煤气</div>
+            <div class="theroyExplain top">3. 其他含CH4气源</div>
+            <!-- <div class="theroyExplain top">4. 炼油厂催化裂化干气、炼油厂重整尾气</div>
+            <div class="theroyExplain top">5. 其他含H2气源</div> -->
+            <h2>技术指标</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 原料气： </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用压力：0.4~4.0MPa（表压）</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用温度：常温</div>
+            <div class="theroyExplain top">2. 输出产品：CH4产品 </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CH4纯度：80~99%（V），根据用户不同的用途和要求生产。</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CH4收率：70~95%</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;系统阻力降△P：0.05~0.1MPa</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出温度：常温</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;注：CH4收率与原料组成、压力及产品以及产品H2纯度有关。</div>
+          </article>
+        </div>
+      </div>
+      <div class="productDetails-contentRight" v-if="techDetail3">
+        <div class="contentRightContent clear">
+          <h3>变压吸附提纯一氧化碳技术</h3>
+          <article class="contentRightWrap">
+            <h2>工艺原理</h2>
+            <div class="theroyExplain" style="margin-top: 20px;">
+               采用变压吸附工艺从含一氧化碳、氢气、氮气、甲烷、二氧化碳等组分的混合气中提纯一氧化碳。原料气进入一段PSA装置，吸附除去二氧化碳、水分及微量硫；
+               脱碳后的净化气进入二段PSA装置，脱除氢气、氮气及甲烷等杂质，吸附的一氧化碳经减压真空解吸作为产品输出。
+            </div>
+            <h2>特点及优势</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 实现了多塔任意组合和任意切换的专家控制及自适应优化控制，既提高了装置操作灵活性，又确保了PSA装置的长期、稳定、安全运行。</div>
+            <div class="theroyExplain top"> 2. 根据原料气的组分、产品气要求，常规吸附剂和专用吸附剂灵活搭配使用，无需升降温实现CO提纯。</div>
+            <!-- <div class="theroyExplain top"> 3. PSA工程化业绩多，工程经验丰富。</div> -->
+            <h2>原料气</h2>
+            <div class="theroyExplain top" style="margin-top: 20px;">1. 水煤气、半水煤气、铜洗再生气、德士古炉气</div>
+            <div class="theroyExplain top">2. 高炉气、转炉气、黄磷尾气，炭黑尾气</div>
+            <div class="theroyExplain top">3. 其他含气源</div>
+            <!-- <div class="theroyExplain top">4. 炼油厂催化裂化干气、炼油厂重整尾气</div>
+            <div class="theroyExplain top">5. 其他含H2气源</div> -->
+            <h2>技术指标</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 原料气： </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用压力：0.1~4.0MPa（表压）</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用温度：常温</div>
+            <div class="theroyExplain top">2. 输出产品：CO </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CO纯度：≥96~99.9%（V），根据用户不同的用途和要求生产。</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CO收率：60~90%</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;主要杂质：N2</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出压力：常压</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出温度：常温</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;注：CO收率与原料组成、压力及产品以及产品CO纯度有关。</div>
+          </article>
+        </div>
+      </div>
+      <div class="productDetails-contentRight" v-if="techDetail4">
+        <div class="contentRightContent clear">
+          <h3>天然气制氢技术</h3>
+          <article class="contentRightWrap">
+            <h2>工艺原理</h2>
+            <div class="theroyExplain" style="margin-top: 20px;">
+                以天然气为原料，经压缩、脱硫后与水蒸气进行转化、变换反应，制得氢含量大于70%（v/v%）以上的富氢转化气。富氢转化气经变压吸附(PSA)单元分离提纯即得到纯度99.9%（v/v%）以上的产品氢气。
+            </div>
+            <h2>特点及优势</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 设计化工甲级资质，拥有PRO/II、HYSYS、CAESARII、ANSYS等一系列专业设计软件，特别是三维设计软件PDMS可减少业主的采购成本，节省投资。</div>
+            <div class="theroyExplain top"> 2. 根据业主需求，采取多种灵活方式进行工程合作。如工程总承包（EPC）、工程设计+提供关键设备（如转化炉）、
+              工程设计+提供部分硬件（脱硫剂、催化剂、吸附剂和程控阀等）、单独工程设计。
+            </div>
+            <div class="theroyExplain top">3. 在核心设备转化炉的设计上拥有丰富的设计能力，成熟的设计经验。在转化炉设备各个方面的选材上保证质量，设计上采用高空速，低水碳比，
+              减少了催化剂的装填，减少了转化炉管的根数，直接减少了转化炉辐射室的外形尺寸，降低了工程投资。
+            </div>
+            <div class="theroyExplain top">4. 采用我公司自行研究开发批量生产的转化催化剂，具有高比表面、高活性、高强度、高碳空速和低水碳比操作的特性，在国内气态烃蒸汽转化制氢装置、
+              甲醇合成气及制氨合成气装置中市场占有率达到85%以上，使用寿命长等优点。
+            </div>
+            <div class="theroyExplain top">回收变压吸附解吸气作为转化炉的燃料，可降低燃料气消耗。采用独特的双通道（天然气、解吸气）低压燃烧器，解吸气回收做转化炉燃料不需设置解吸气压缩机或鼓风机，并已有多套同类装置业绩。</div>
+            <div class="theroyExplain top"> 6. 制氢装置中采用的天然气脱硫剂、吸附剂均为我公司自行研究开发批量生产。</div>
+            <div class="theroyExplain top"> 7. 提供优质的催化剂升温还原及装置开车技术指导服务。</div>
+            <h2>装置规模</h2>
+            <div class="theroyExplain top" style="margin-top: 20px;">400~200000 Nm3/h</div>
+            <!-- <div class="theroyExplain top">2. 高炉气、转炉气、黄磷尾气，炭黑尾气</div>
+            <div class="theroyExplain top">3. 其他含气源</div>
+            <h2>技术指标</h2>
+            <div class="theroyExplain" style="margin-top: 20px;"> 1. 原料气： </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用压力：0.1~4.0MPa（表压）</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;适用温度：常温</div>
+            <div class="theroyExplain top">2. 输出产品：CO </div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CO纯度：≥96~99.9%（V），根据用户不同的用途和要求生产。</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;CO收率：60~90%</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;主要杂质：N2</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出压力：常压</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;输出温度：常温</div>
+            <div class="theroyExplain top">&nbsp;&nbsp;&nbsp;&nbsp;注：CO收率与原料组成、压力及产品以及产品CO纯度有关。</div> -->
           </article>
         </div>
       </div>
@@ -103,6 +207,25 @@
 export default {
   data () {
     return {
+      techDetail1: false,
+      techDetail2: false,
+      techDetail3: false,
+      techDetail4: false
+    }
+  },
+  mounted () {
+    var self = this
+    var query = self.$route.query
+    if (query.tech === 1) {
+      self.techDetail1 = true
+    } else if (query.tech === 2) {
+      self.techDetail2 = true
+    } else if (query.tech === 3) {
+      self.techDetail3 = true
+    } else if (query.tech === 4) {
+      self.techDetail4 = true
+    } else if (query.tech === 5) {
+      self.techDetail5 = true
     }
   },
   computed: {
@@ -112,6 +235,30 @@ export default {
 		},
   },
    methods: {
+    techDetail (math) {
+      var self = this
+      if (math === 1) {
+        self.techDetail1 = true
+        self.techDetail2 = false
+        self.techDetail3 = false
+        self.techDetail4 = false
+      } else if (math === 2) {
+        self.techDetail2 = true
+        self.techDetail1 = false
+        self.techDetail3 = false
+        self.techDetail4 = false
+      } else if (math === 3) {
+        self.techDetail3 = true
+        self.techDetail2 = false
+        self.techDetail4 = false
+        self.techDetail1 = false
+      } else if (math === 4) {
+        self.techDetail3 = false
+        self.techDetail2 = false
+        self.techDetail1 = false
+        self.techDetail4 = true
+      }
+    },
     handleOpen(key, keyPath) {
       // console.log(key, keyPath);
     },
@@ -144,7 +291,7 @@ export default {
   color: #fff!important;
 }
   .techDetails {
-    height: 1650px;
+    // height: 1650px;
     .productDetails-contentLeft {
       margin-top: 30px;
       .techDetails-content {
